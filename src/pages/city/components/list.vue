03c005c28ea8d6a41f3a7a588be3e68a3396888a
CGNz <32739359+CGNz@users.noreply.github.com>
2018-08-03 16:56:35 +0800

    component-index
    父子组件关联

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -21,7 +21,9 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item,key) of cities" :key="key">
+            <div class="area" v-for="(item,key) of cities" :key="key"
+                :ref="key"
+            >
                 <div class="title border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">
@@ -41,10 +43,20 @@ export default {
     name: 'Citylist',
     props:{
         hot: Array,
-        cities: Object
+        cities: Object,
+        letter: String
     },
     mounted (){
         this.scroll = new Bscroll(this.$refs.wrapper)
+    },
+    watch:{
+        letter() {
+            //console.log(this.letter)
+            if (this.letter){
+                const element = this.$refs[this.letter][0]
+                this.scroll.scrollToElement(element)
+            }
+        }
     }
 }
 </script>
